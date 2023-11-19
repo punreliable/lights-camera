8d5bfa4762ab3146674352be259031eb9af25676
Brian <picketfence14@gmail.com>
2023-11-19 12:42:21 -0500

    Icons have been updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import reactImg from '../../assets/react-core-concepts.png';
+import reactImgFlat from '../../assets/icons/button.svg';
 import './Header.css';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
@@ -12,7 +13,7 @@ export default function Header() {
 
   return (
     <header>
-      <img src={reactImg} alt="Stylized atom" />
+      <img src={reactImgFlat} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
--- src/data.js
+++ src/data.js
@@ -3,27 +3,32 @@ import propsImg from './assets/config.png';
 import jsxImg from './assets/jsx-ui.png';
 import stateImg from './assets/state-mgmt.png';
 
+import componentsImgFlat from './assets/icons/box.svg';
+import propsImgFlat from './assets/icons/magic.svg';
+import jsxImgFlat from './assets/icons/responsive.svg';
+import stateImgFlat from './assets/icons/loop.svg';
+
 export const CORE_CONCEPTS = [
   {
-    image: componentsImg,
+    image: componentsImgFlat,
     title: 'Components',
     description:
       'The core UI building block - compose the user interface by combining multiple components.',
   },
   {
-    image: jsxImg,
+    image: jsxImgFlat,
     title: 'JSX',
     description:
       'Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.',
   },
   {
-    image: propsImg,
+    image: propsImgFlat,
     title: 'Props',
     description:
       'Make components configurable (and therefore reusable) by passing input data to them.',
   },
   {
-    image: stateImg,
+    image: stateImgFlat,
     title: 'State',
     description:
       'React-managed data which, when changed, causes the component to re-render & the UI to update.',
